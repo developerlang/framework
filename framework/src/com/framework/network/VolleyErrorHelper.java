cee8ea47a2686bcf5298f9d8b49ed925e54108b5
jacky zhang <zhangjb@chanjet.com>
2014-10-20 16:50:46 +0800

    add adapter helper

--- framework/src/com/framework/network/VolleyErrorHelper.java
+++ framework/src/com/framework/network/VolleyErrorHelper.java
@@ -1,3 +1,4 @@
+
 package com.framework.network;
 
 import android.content.Context;
@@ -16,81 +17,81 @@ import com.google.gson.reflect.TypeToken;
 import java.util.HashMap;
 import java.util.Map;
 
-public class VolleyErrorHelper{
-    
-  //AuthFailureError：如果在做一个HTTP的身份验证，可能会发生这个错误。  
-  //NetworkError：Socket关闭，服务器宕机，DNS错误都会产生这个错误。  
-  //NoConnectionError：和NetworkError类似，这个是客户端没有网络连接。  
-  //ParseError：在使用JsonObjectRequest或JsonArrayRequest时，如果接收到的JSON是畸形，会产生异常。  
-  //SERVERERROR：服务器的响应的一个错误，最有可能的4xx或5xx HTTP状态代码。  
-  //TimeoutError：Socket超时，服务器太忙或网络延迟会产生这个异常。默认情况下，Volley的超时时间为2.5秒。如果得到这个错误可以使用RetryPolicy。
- /** 
- * Returns appropriate message which is to be displayed to the user against 
- * the specified error object. 
- */  
-public static String getMessage(Object error, Context context) {  
-    if (error instanceof TimeoutError) {  
-        return context.getResources().getString(  
-                R.string.generic_server_down);  
-    } else if (isServerProblem(error)) {  
-        return handleServerError(error, context);  
-    } else if (isNetworkProblem(error)) {  
-        return context.getResources().getString(R.string.no_internet);  
-    }  
-    return context.getResources().getString(R.string.generic_error);  
-}  
+public class VolleyErrorHelper {
 
-/** 
- * Determines whether the error is related to network 
- */  
-private static boolean isNetworkProblem(Object error) {  
-    return (error instanceof NetworkError)  
-            || (error instanceof NoConnectionError);  
-}  
+    // AuthFailureError：如果在做一个HTTP的身份验证，可能会发生这个错误。
+    // NetworkError：Socket关闭，服务器宕机，DNS错误都会产生这个错误。
+    // NoConnectionError：和NetworkError类似，这个是客户端没有网络连接。
+    // ParseError：在使用JsonObjectRequest或JsonArrayRequest时，如果接收到的JSON是畸形，会产生异常。
+    // SERVERERROR：服务器的响应的一个错误，最有可能的4xx或5xx HTTP状态代码。
+    // TimeoutError：Socket超时，服务器太忙或网络延迟会产生这个异常。默认情况下，Volley的超时时间为2.5秒。如果得到这个错误可以使用RetryPolicy。
+    /**
+     * Returns appropriate message which is to be displayed to the user against
+     * the specified error object.
+     */
+    public static String getMessage(Object error, Context context) {
+        if (error instanceof TimeoutError) {
+            return context.getResources().getString(
+                    R.string.generic_server_down);
+        } else if (isServerProblem(error)) {
+            return handleServerError(error, context);
+        } else if (isNetworkProblem(error)) {
+            return context.getResources().getString(R.string.no_internet);
+        }
+        return context.getResources().getString(R.string.generic_error);
+    }
 
-/** 
- * Determines whether the error is related to server 
- */  
-private static boolean isServerProblem(Object error) {  
-    return (error instanceof ServerError)  
-            || (error instanceof AuthFailureError);  
-}  
+    /**
+     * Determines whether the error is related to network
+     */
+    private static boolean isNetworkProblem(Object error) {
+        return (error instanceof NetworkError)
+                || (error instanceof NoConnectionError);
+    }
 
-/** 
- * Handles the server error, tries to determine whether to show a stock 
- * message or to show a message retrieved from the server. 
- */  
-private static String handleServerError(Object err, Context context) {  
-    VolleyError error = (VolleyError) err;  
+    /**
+     * Determines whether the error is related to server
+     */
+    private static boolean isServerProblem(Object error) {
+        return (error instanceof ServerError)
+                || (error instanceof AuthFailureError);
+    }
 
-    NetworkResponse response = error.networkResponse;  
+    /**
+     * Handles the server error, tries to determine whether to show a stock
+     * message or to show a message retrieved from the server.
+     */
+    private static String handleServerError(Object err, Context context) {
+        VolleyError error = (VolleyError) err;
 
-    if (response != null) {  
-        switch (response.statusCode) {  
-        case 404:  
-        case 422:  
-        case 401:  
-            try {  
-                HashMap<String, String> result = new Gson().fromJson(  
-                        new String(response.data),  
-                        new TypeToken<Map<String, String>>() {  
-                        }.getType());  
+        NetworkResponse response = error.networkResponse;
 
-                if (result != null && result.containsKey("error")) {  
-                    return result.get("error");  
-                }  
+        if (response != null) {
+            switch (response.statusCode) {
+                case 404:
+                case 422:
+                case 401:
+                    try {
+                        HashMap<String, String> result = new Gson().fromJson(
+                                new String(response.data),
+                                new TypeToken<Map<String, String>>() {
+                                }.getType());
 
-            } catch (Exception e) {  
-                e.printStackTrace();  
-            }  
-            // invalid request  
-            return error.getMessage();  
+                        if (result != null && result.containsKey("error")) {
+                            return result.get("error");
+                        }
 
-        default:  
-            return context.getResources().getString(  
-                    R.string.generic_server_down);  
-        }  
-    }  
-    return context.getResources().getString(R.string.generic_error);  
-}  
-}  
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    // invalid request
+                    return error.getMessage();
+
+                default:
+                    return context.getResources().getString(
+                            R.string.generic_server_down);
+            }
+        }
+        return context.getResources().getString(R.string.generic_error);
+    }
+}
