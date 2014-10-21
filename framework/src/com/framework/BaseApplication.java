46e4e713bcbf9cbc44a318df172ae12a1e2e2a59
jacky zhang <zhangjb@chanjet.com>
2014-10-21 16:53:36 +0800

    add Utils

--- framework/src/com/framework/BaseApplication.java
+++ framework/src/com/framework/BaseApplication.java
@@ -2,17 +2,26 @@
 package com.framework;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.framework.network.RequestManager;
 
 public class BaseApplication extends Application {
+
+    private static Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext = this;
         init();
     }
 
     private void init() {
         RequestManager.init(this);
+    }
+    
+    public static Context getContext() {
+        return mContext;
     }
 }
